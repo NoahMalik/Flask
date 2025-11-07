eff9d4661eee197542304b4ccfa0fe800bb27415
NoahMalik <nhmkb9@umsystem.edu>
2025-11-07 13:23:03 -0600

    updating the app

--- app.py
+++ app.py
@@ -1,33 +1,88 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
-# make a Flask application object called app
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'notasecret'
 
 
-
-# Function to open a connection to the database.db file
 def get_db_connection():
-    # create connection to the database
     conn = sqlite3.connect('database.db')
-    
-    # allows us to have name-based access to columns
-    # the database connection will return rows we can access like regular Python dictionaries
     conn.row_factory = sqlite3.Row
-
-    #return the connection object
     return conn
 
 
-# use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
-    
-    return "<h1>Welcome to My Blog</h1>"
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts ORDER BY created DESC').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 
-# route to create a post
+@app.route('/<int:id>')
+def post(id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return render_template('post.html', post=post)
 
 
-app.run()
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+
+    if post is None:
+        abort(404)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    conn.close()
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    if post is None:
+        abort(404)
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash(f'"{post["title"]}" was successfully deleted!')
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
